da164365131186183383a62e45772111f86729e3
Ryan <hello@ryanwu.me>
2017-09-13 11:33:58 -0400

    Allow Content-Range Header

--- app-crud/index.js
+++ app-crud/index.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Expose-Headers', 'Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma, Content-Range');
   next();
 });
 
