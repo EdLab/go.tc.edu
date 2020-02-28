93fd8791260ccef7105c7899ad343e929f4e4d09
Sabarish Raghupathy <sabarishraghupathy@gmail.com>
2020-02-28 16:11:59 -0500

    database ssl update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const fs = require('fs')
+const rdsCa = fs.readFileSync(`${__dirname}/rds-ca-2019-root.pem`)
 
 const {
   DB_USERNAME,
@@ -15,18 +17,20 @@ debug(`Running on database ${DB_DATABASE}`);
 let sequelize = new Sequelize(DB_DATABASE,
   DB_USERNAME,
   DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-    dialectOptions: DB_SSL_ENABLED === 'true' ? {
-      ssl: 'Amazon RDS',
-    } : null,
-    logging: debug,
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 30
-    }
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  dialectOptions: {
+    ssl: {
+      ca: rdsCa,
+    },
+  },
+  logging: debug,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 30
   }
+}
 );
 
 module.exports = sequelize;
